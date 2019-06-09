ab72376e6604a4b76e09f248bcfb290e664582f0
OthankQ <ryan@Ryanui-Mac-mini.local>
2019-06-09 20:12:01 +0900

    add datepicker, label

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblSelectedTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        
+        let date = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblSelectedTime.text = "Selected Time: \(formatter.string(from:date as Date))"
+        
     }
 
-
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblSelectedTime.text = "Selected Time: \(formatter.string(from: datePickerView.date))"
+    }
+    
+    @objc func updateTime() {
+//        lblCurrentTime.text = String(count)
+//        count += 1
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "Current Time: \(formatter.string(from: date as Date))"
+    }
 }
 
